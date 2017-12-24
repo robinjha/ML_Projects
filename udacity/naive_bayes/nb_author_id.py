c14f2dc9942634b940bc79f8d9df6b30aaaba6c8
robinjha <robinjha@gmail.com>
2017-12-24 11:48:41 -0800

    GaussianNB implementation, accuracy score and training and prediction time calculations

--- udacity/naive_bayes/nb_author_id.py
+++ udacity/naive_bayes/nb_author_id.py
@@ -15,6 +15,7 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,8 +27,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0,3),"s"
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1,3),"s"
+accuracy = accuracy_score(labels_test, pred)
+#format(accuracy, '.2f')
+print("Accuracy : %.2f" %(accuracy))
 #########################################################
 
 
