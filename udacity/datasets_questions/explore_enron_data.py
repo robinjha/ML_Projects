1f896fc0addd7d3cfae6d1635def0bb89b10b6ec
robinjha <robinjha@gmail.com>
2017-12-29 18:12:37 -0800

    dataset code

--- udacity/datasets_questions/explore_enron_data.py
+++ udacity/datasets_questions/explore_enron_data.py
@@ -20,17 +20,23 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 print("Data: %d" %len(enron_data["GLISAN JR BEN F"].keys()))
-count = 0
-for k,v in enron_data.iteritems():
-	if enron_data[k]["poi"]==1:
-		count = count + 1
+salary_count = 0
+email_count = 0
+total_payment_na = 0
 
-print("POI Count:  %d" %count)
+for k,v in enron_data.iteritems():
+	if enron_data[k]["salary"]!='NaN':
+		salary_count = salary_count + 1
+	if enron_data[k]['email_address']!='NaN':
+		email_count = email_count + 1
+	if enron_data[k]['total_payments']=='NaN':
+		total_payment_na = total_payment_na +1
+print ("Total Number : %d"%len( enron_data.keys()))
+print("Salary Count:  %d \t Email Count: %d \t Total NaN : %d \t Percentage : %.2f)" %(salary_count, email_count, total_payment_na, total_payment_na/len(enron_data.keys())))
 num_lines = sum(1 for line in open('../final_project/poi_names.txt'))
-print(enron_data)
 print(enron_data["SKILLING JEFFREY K"])
-print(enron_data["LAY KENNETH"])
-print(enron_data["FASTOW ANDREW"])
+print(enron_data["LAY KENNETH L"])
+print(enron_data["FASTOW ANDREW S"])
 print("Line Count : %d" %num_lines )
 #print(enron_data["PRENTICE JAMES"]["total_stock_value"])
 print("Value of Stocks for James Prentice : %d" %enron_data["PRENTICE JAMES"]["total_stock_value"])
